8ac880eacd7daeff1a3844d276a2e484d67d5adb
NivenT <nachenjang@gmail.com>
2021-07-21 23:12:00 -0400

    everything but notes

---
+++ assets/js/secret.js
@@ -0,0 +1,21 @@
+function konami(callback) {
+  let keys = [];
+  const konami = '38,38,40,40,37,39,37,39,66,65,13';
+  return (event) => {
+    keys.push(event.keyCode);
+    if (keys.toString().indexOf(konami) >= 0) {
+      keys = [];
+      callback();
+    }
+  };
+}
+
+window.addEventListener(
+  'keydown',
+  konami(() => {
+    const msg = `Congrats! As a reward, pres 'OK' to see a long-kept-secret video I was in from the early days of YouTube (or hit 'Esc' to ignore this).`;
+    if (window.confirm(msg)) {
+      window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+    }
+  })
+);
