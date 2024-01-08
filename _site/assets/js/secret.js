75727d4e513d723f48ebc1aa4d6a3b383522ae15
NivenT <nachenjang@gmail.com>
2024-01-08 11:46:17 -0500

    build

--- _site/assets/js/secret.js
+++ _site/assets/js/secret.js
@@ -1,3 +1,7 @@
+function boop() {
+  console.log("Boop!");
+}
+
 function konami(callback) {
   let keys = [];
   const konami = '38,38,40,40,37,39,37,39,66,65,13';
@@ -13,9 +17,10 @@ function konami(callback) {
 window.addEventListener(
   'keydown',
   konami(() => {
-    const msg = `Congrats! As a reward, pres 'OK' to see a long-kept-secret video I was in from the early days of YouTube (or hit 'Esc' to ignore this).`;
+    const msg = `Congrats! As a reward, press 'OK' to see a long-kept-secret video I was in from the early days of YouTube (or hit 'Esc' to ignore this).`;
     if (window.confirm(msg)) {
       window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
     }
   })
 );
+
